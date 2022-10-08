4d96b06cf942aed5a402dcf997c39b7b1bcdf1da
BiniyamAschalew <111734030+BiniyamAschalew@users.noreply.github.com>
2022-10-08 19:34:20 +0900

    Time: 212 ms (45.55%), Space: 14.6 MB (83.60%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -1,4 +1,8 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
-        # we need at least k deci-binary if k is the maximum digit in a number 
-        return int(max(list(n)))
+        # we need at least k deci-binary if k is the maximum digit in a number
+        nums = 0
+        for character in n:
+            if int(character) > nums:
+                nums = int(character)
+        return nums
